27db2abde56ca5c808b2bf452dacd4d55fef71de
Wei <weijingyu@outlook.com>
2022-08-11 19:36:53 +0800

    update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,6 @@
 project "glfw"
     kind "StaticLib"
     language "C"
-    staticruntime "off"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -15,43 +14,20 @@ project "glfw"
         "src/init.c",
         "src/input.c",
         "src/monitor.c",
-
-        "src/null_init.c",
-        "src/null_joystick.c",
-        "src/null_monitor.c",
-        "src/null_window.c",
-
-        "src/platform.c",
-        "src/vulkan.c",
         "src/window.c",
+
+        -- "src/null_init.c",
+        -- "src/null_joystick.c",
+        -- "src/null_monitor.c",
+        -- "src/null_window.c",
+
+        -- "src/platform.c",
+        -- "src/vulkan.c",
     }
-
-    filter "system:linux"
-        pic "On"
-
-        systemversion "latest"
-        
-        files
-        {
-            "src/x11_init.c",
-            "src/x11_monitor.c",
-            "src/x11_window.c",
-            "src/xkb_unicode.c",
-            "src/posix_time.c",
-            "src/posix_thread.c",
-            "src/glx_context.c",
-            "src/egl_context.c",
-            "src/osmesa_context.c",
-            "src/linux_joystick.c"
-        }
-
-        defines
-        {
-            "_GLFW_X11"
-        }
 
     filter "system:windows"
         systemversion "latest"
+        staticruntime "On"
 
         files
         {
@@ -73,10 +49,10 @@ project "glfw"
             "_CRT_SECURE_NO_WARNINGS"
         }
 
-        links
-        {
-            "Dwmapi.lib"
-        }
+        -- links
+        -- {
+        --     "Dwmapi.lib"
+        -- }
 
     filter "configurations:Debug"
         runtime "Debug"
